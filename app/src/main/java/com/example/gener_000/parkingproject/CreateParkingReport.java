91c746e80b8f105272638043f5a21a29b8a8b96a
Mark <Maerickson@cpp.edu>
2016-10-29 17:00:49 -0700

    Origin. Some errors

---
+++ app/src/main/java/com/example/gener_000/parkingproject/CreateParkingReport.java
@@ -0,0 +1,13 @@
+package com.example.gener_000.parkingproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CreateParkingReport extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
