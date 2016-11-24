b703ef9a5e873e82001b91fdcf1ddff8e4389e3a
Mark <Maerickson@cpp.edu>
2016-11-23 23:16:48 -0800

    Added graph and maps page

---
+++ app/src/main/java/com/example/gener_000/parkingproject/PictureOfMap.java
@@ -0,0 +1,13 @@
+package com.example.gener_000.parkingproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PictureOfMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture_of_map);
+    }
+}
